fe34bccf96624998c42524239a16b22681f7df99
Eyand Daniel Eyoh <daniel.eyang.ed@gmail.com>
2025-06-07 00:33:12 +0100

    FIXES: setup dev to debug deploy issues

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
+  deployment: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: "mysql"
+  },
   production: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
